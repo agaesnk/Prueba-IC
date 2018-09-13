0bb372fa8b2adbb5399d7d483c17d1da4a72b6ae
agaesnk <agaesnk@gmail.com>
2018-09-13 15:27:48 +0200

    post component created

---
+++ components/Post.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Moment from 'moment';
+
+export default class App extends React.Component {
+  render() {
+
+    const item = this.props;
+    const relativeDate = Moment(item.data.created_utc).fromNow();
+    const postTitle = item.data.title;
+    const postImage = item.data.thumbernails;
+    const postAuthor = item.data.author;
+    const postScore = item.data.score;
+    const postComments = item.data.comments;
+
+    return (
+      <View style={styles.mainContainer}>
+        <image source = {{uri: postImage}}/>
+        <View style={styles.Container}>
+          <Text>{ relativeDate }</Text>
+          <Text>{ postTitle }</Text>
+          <View style={styles.subContainer}>
+            <Text>Author: { postAuthor }</Text>
+            <Text>Score: { postScore }</Text>
+            <Text>Comments: { postComments }</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+
